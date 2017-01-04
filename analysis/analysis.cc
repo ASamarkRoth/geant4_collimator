2b6af08b44ce448a2935494791fe2058571fb95e
Anton Roth <antonrothus@gmail.com>
2017-01-04 07:56:54 +0100

    This is the version after Sthlm. Analysis finished. Error to be investigated @xi.

--- analysis/analysis.cc
+++ analysis/analysis.cc
@@ -18,13 +18,14 @@ int main(int argc, char** argv) {
 	double R[5] = {1, 2, 4, 6, 10};
 	double L[5] = {10, 20, 33, 50, 100};
 
-//	int full[5][5];
-//	int all[5][5];
-//	double scatter[5][5];
+	int full[5][5];
+	int all[5][5];
+	double scatter[5][5];
+	double sigma[5][5];
 
-	int full[5];
-	int all[5];
-	double scatter[5];
+//	int full[5];
+//	int all[5];
+//	double scatter[5];
 
 	string file_name;
 	TFile* root_file;
@@ -33,35 +34,76 @@ int main(int argc, char** argv) {
 
 	//TGraph* g_scatter[5];
 	TGraph* g_scatter;
+	int nbr_gammas = 0;
+	//Box size = 20x20 mm², nbr_bins = 200 -> bin_width=0.1 mm and bin_area = 0.01 mm² = 1e-4 cm² = 0.0001 cm²
+	double factor = 0.0001;
 
 	for(int i=0;i<5;i++) {
 		for(int j=0;j<5;j++) {
-			file_name = "L"+to_string(int(L[i]))+"R"+to_string(int(R[j]))+".root";
-			cout << file_name << endl;
+			file_name = "FL"+to_string(int(L[i]))+"R"+to_string(int(R[j]))+".root";
+			//cout << file_name << endl;
 			root_file = new TFile(TString(PWD+file_name));
 			hist = (TH2D*) root_file->Get("boxMesh_1");
 			if(!hist) cout << "Histogram not found"<<endl;
-			//all[i][j] = hist->GetEntries();
-			all[j] = hist->GetEntries();
-			cout << all[j] << endl;
+			/*
+			for(int y = 0; y < hist->GetNbinsY(); y++) {
+				for(int x = 0; x < hist->GetNbinsX(); x++) {
+					nbr_gammas += hist->GetBinContent(x,y)*factor;
+				}
+			}
+			cout << "Gammas = " << nbr_gammas << endl;
+			*/
+			all[i][j] = hist->GetEntries();
 
 			file_name = "fullE_"+file_name;
 			root_file = new TFile(TString(PWD+file_name));
 			hist = (TH2D*) root_file->Get("boxMesh_1");
 			if(!hist) cout << "Histogram not found"<<endl;
 
-			//full[i][j] = hist->GetEntries();
-			full[j] = hist->GetEntries();
-			cout << full[j] << endl;
+			full[i][j] = hist->GetEntries();
 
-			//scatter[i][j] = (double) full[i][j]/all[i][j];
-			//cout << scatter[i][j] << endl;
-			scatter[j] = (double) full[j]/all[j];
-			cout << scatter[j] << endl;
+			scatter[i][j] = (double) full[i][j]/all[i][j];
+			sigma[i][j] = (hist->GetStdDev()+hist->GetStdDev(2))/2;
 		}
-		g_scatter = new TGraph(5, R, scatter);
+		g_scatter = new TGraph(5, R, scatter[i]);
 		g_scatter->SetName(TString(file_name));
 	}
+
+	cout << "AllE = np.asarray([[";
+	for(int i=0;i<5;i++) {
+		if(i > 0) cout << "[";
+		for(int j=0;j<5;j++) {
+			cout << all[i][j];
+			if(j < 4) cout << ", ";
+		}
+		cout << "]";
+		if(i < 4) cout << ", " << endl;
+	}
+	cout << "])" << endl;
+
+	cout << "FullE = np.asarray([[";
+	for(int i=0;i<5;i++) {
+		if(i > 0) cout << "[";
+		for(int j=0;j<5;j++) {
+			cout << full[i][j];
+			if(j < 4) cout << ", ";
+		}
+		cout << "]";
+		if(i < 4) cout << ", " << endl;
+	}
+	cout << "])" << endl;
+
+	cout << "sigma = np.asarray([[";
+	for(int i=0;i<5;i++) {
+		if(i > 0) cout << "[";
+		for(int j=0;j<5;j++) {
+			cout << sigma[i][j];
+			if(j < 4) cout << ", ";
+		}
+		cout << "]";
+		if(i < 4) cout << ", " << endl;
+	}
+	cout << "])" << endl;
 
 	cout << g_scatter->GetMean() << endl;
 
@@ -73,8 +115,8 @@ int main(int argc, char** argv) {
 		b[j]=j;
 	}
 
-	TGraph* bajs = new TGraph(3, a, b);
-	bajs->Draw();
+	//TGraph* bajs = new TGraph(3, a, b);
+	//bajs->Draw();
 	cout << scatter[0] << endl;
 	for(int j=0;j<1;j++) {
 		//g_scatter->Draw("SAME C");
--- analysis/analysis.py
+++ analysis/analysis.py
@@ -25,15 +25,63 @@ sigma = np.asarray([[2.98, 5.37, 10.3, 15.2, 24.86],
                 [3.24, 4.45, 6.89, 9.26, 13.7],
                 ]);
 
+L = np.asarray([100, 50, 33, 20, 10]);#y row
+L = L[::-1]
+R = np.asarray([1, 2, 4, 6, 10]); #x column
+
+AllE = np.asfarray([[33750, 64802, 126447, 202434, 393838], 
+    [35204, 69641, 133269, 217532, 427571], 
+    [35707, 78299, 145427, 236140, 462980], 
+    [35616, 106171, 174391, 271468, 520635], 
+    [35740, 121676, 443975, 969824, 2501025]])
+FullE = np.asfarray([[30925, 59125, 111400, 176200, 332975], 
+    [32025, 65650, 122050, 193150, 372400], 
+    [32550, 74125, 136350, 219125, 418575], 
+    [32525, 104425, 163725, 253575, 485700], 
+    [32675, 115350, 425325, 932151, 2500443]])
+sigma = np.asfarray([[3.2453, 4.45339, 6.89529, 9.26626, 13.7001], 
+    [2.96862, 4.57872, 6.85954, 9.24187, 13.8913], 
+    [2.97827, 4.77507, 7.07675, 9.31231, 14.0341], 
+    [3.02411, 5.23447, 7.55976, 9.68726, 14.4091], 
+    [2.98459, 5.37735, 10.3066, 15.1945, 24.8626]])
+
+#Flipped cone
+FAllE = np.asfarray([[33700, 63536, 122589, 193504, 369130], 
+    [34001, 61359, 117950, 186212, 350234], 
+    [34157, 61295, 111347, 175541, 320578], 
+    [34332, 57055, 102759, 159352, 292394], 
+    [35742, 47234, 78681, 121241, 221317]])
+FFullE = np.asfarray([[30800, 55025, 100125, 158000, 296625], 
+    [31400, 51850, 94300, 147550, 271600], 
+    [31625, 49625, 87275, 134275, 242050], 
+    [31750, 46225, 79125, 119125, 211975], 
+    [32650, 40200, 58425, 86125, 149275]])
+Fsigma = np.asfarray([[3.25015, 4.41408, 6.80587, 9.12657, 13.3619], 
+    [3.21977, 4.34588, 6.74119, 9.09396, 13.1437], 
+    [3.11863, 4.3776, 6.67667, 8.90469, 12.7843], 
+    [3.11468, 4.33032, 6.54454, 8.73531, 12.456], 
+    [2.98526, 4.38421, 6.69438, 8.42766, 12.1809]])
+
+sigma = sigma*0.1*3
+Fsigma = Fsigma*0.1*3
+
 scatter = (1.0*FullE)/AllE
+Fscatter = (1.0*FFullE)/FAllE
 
 errors_num = np.sqrt(FullE);
 errors_den = np.sqrt(AllE);
 
+beam_total = 2500919
+
+solid_angle = 5.**2/(4*100.**2)
+
 legend_labels = ["100", "50", "33", "20", "10"]
+colors = ["y", "b", "g", "r", "k"]
+legend_labels = legend_labels[::-1]
+
 plt.figure()
 for j in range(len(L)):
-    plt.plot(R, scatter[j,:], linestyle='-', marker='o', markersize=10, label = legend_labels[j])
+    plt.plot(R, scatter[j,:], color = colors[j], linestyle='-', marker='o', markersize=10, label = legend_labels[j])
 ax = plt.gca()
 handles, labels = ax.get_legend_handles_labels()
 ax.legend(handles, legend_labels, loc=1, title="Cone length (mm)")
@@ -42,39 +90,117 @@ plt.xlabel("Cone radius (mm)", fontsize=18)
 plt.ylabel("Scattering efficiency", fontsize=18)
 save_string = "scattering.pdf"
 plt.savefig(save_string, format='pdf', dpi=1000)
-plt.show()
+#plt.show()
 plt.close()
 
-legend_labels = ["100", "50", "33", "20", "10"]
 plt.figure()
 for j in range(len(L)):
-    plt.plot(R, sigma[j,:], linestyle='-', marker='o', markersize=10, label = legend_labels[j])
+    plt.plot(R, sigma[j,:], color = colors[j], linestyle='-', marker='o', markersize=10, label = legend_labels[j])
 ax = plt.gca()
 handles, labels = ax.get_legend_handles_labels()
 ax.legend(handles, legend_labels, loc=1, title="Cone length (mm)")
 #plt.legend.set_title("Cone length (mm)")
 plt.xlabel("Cone radius (mm)", fontsize=18)
-plt.ylabel("Beam divergence (arb. unit)", fontsize=18)
+plt.ylabel("Beam divergence, 3*StdDev (mm)", fontsize=18)
 save_string = "divergence.pdf"
+plt.savefig(save_string, format='pdf', dpi=1000)
+#plt.show()
+plt.close()
+
+plt.figure()
+for j in range(len(L)):
+    plt.plot(R, Fscatter[j,:], color = colors[j], linestyle='-', marker='o', markersize=10, label = legend_labels[j])
+ax = plt.gca()
+handles, labels = ax.get_legend_handles_labels()
+ax.legend(handles, legend_labels, loc=1, title="Cone length (mm)")
+#plt.legend.set_title("Cone length (mm)")
+plt.title("Cone flipped", fontsize=20)
+plt.xlabel("Cone radius (mm)", fontsize=18)
+plt.ylabel("Scattering efficiency", fontsize=18)
+save_string = "Fscattering.pdf"
+plt.savefig(save_string, format='pdf', dpi=1000)
+#plt.show()
+plt.close()
+
+plt.figure()
+for j in range(len(L)):
+    plt.plot(R, Fsigma[j,:], color = colors[j], linestyle='-', marker='o', markersize=10, label = legend_labels[j])
+ax = plt.gca()
+handles, labels = ax.get_legend_handles_labels()
+ax.legend(handles, legend_labels, loc=1, title="Cone length (mm)")
+#plt.legend.set_title("Cone length (mm)")
+plt.title("Cone flipped", fontsize=20)
+plt.xlabel("Cone radius (mm)", fontsize=18)
+plt.ylabel("Beam divergence, 3*StdDev (mm)", fontsize=18)
+save_string = "Fdivergence.pdf"
+plt.savefig(save_string, format='pdf', dpi=1000)
+#plt.show()
+plt.close()
+
+plt.figure(figsize=(12,10))
+for j in range(len(L)):
+    plt.plot(R, scatter[j,:], color = colors[j], linestyle='-', marker='o', markersize=10, label = legend_labels[j])
+for j in range(len(L)):
+    plt.plot(R, Fscatter[j,:], color = colors[j], linestyle='--', marker='*', markersize=10, label = legend_labels[j])
+ax = plt.gca()
+handles, labels = ax.get_legend_handles_labels()
+ax.legend(handles, legend_labels, loc=1, title="Cone length (mm)")
+#plt.legend.set_title("Cone length (mm)")
+plt.title("Scattering (both)", fontsize=20)
+plt.xlabel("Cone radius (mm)", fontsize=18)
+plt.ylabel("Scattering efficiency", fontsize=18)
+save_string = "Bscattering.pdf"
+plt.savefig(save_string, format='pdf', dpi=1000)
+#plt.show()
+plt.close()
+
+plt.figure(figsize=(12,10))
+for j in range(len(L)):
+    plt.plot(R, sigma[j,:], color = colors[j], linestyle='-', marker='o', markersize=10, label = legend_labels[j])
+for j in range(len(L)):
+    plt.plot(R, Fsigma[j,:], color = colors[j], linestyle='--', marker='*', markersize=10, label = legend_labels[j])
+ax = plt.gca()
+handles, labels = ax.get_legend_handles_labels()
+ax.legend(handles, legend_labels, loc=1, title="Cone length (mm)")
+#plt.legend.set_title("Cone length (mm)")
+plt.title("Divergence (both)", fontsize=20)
+plt.xlabel("Cone radius (mm)", fontsize=18)
+plt.ylabel("Beam divergence, 3*StdDev (mm)", fontsize=18)
+save_string = "Bdivergence.pdf"
+plt.savefig(save_string, format='pdf', dpi=1000)
+#plt.show()
+plt.close()
+
+plt.figure(figsize=(12,10))
+for j in range(len(L)):
+    plt.plot(R, FullE[j,:], color = colors[j], linestyle='-', marker='o', markersize=10, label = legend_labels[j])
+for j in range(len(L)):
+    plt.plot(R, FFullE[j,:], color = colors[j], linestyle='--', marker='*', markersize=10, label = legend_labels[j])
+ax = plt.gca()
+handles, labels = ax.get_legend_handles_labels()
+ax.legend(handles, legend_labels, loc=1, title="Cone length (mm)")
+#plt.legend.set_title("Cone length (mm)")
+plt.title("Flux", fontsize=20)
+plt.xlabel("Cone radius (mm)", fontsize=18)
+plt.ylabel("Flux (arb. unit)", fontsize=18)
+save_string = "BFlux.pdf"
 plt.savefig(save_string, format='pdf', dpi=1000)
 plt.show()
 plt.close()
 
-"""
-fig = plt.figure()
-for j in range(len(legend_labels)):
-        plt.plot(thickness[0:5], Mix_stop_cryo_cell[j,:][0:5], color = colors[j], linestyle='-', marker='*', markersize=10, label = legend_labels[j])
-        ax = plt.gca()
-        plt.xlim([2.5,4.5])
-        plt.ylim([30, 100])
-        plt.xlabel("Ti-foil thickness ($\mu m$)", fontsize=18)
-        plt.ylabel("Stopping efficiency (%)", fontsize=18)
-        plt.grid()
-        handles, labels = ax.get_legend_handles_labels()
-        ax.legend(handles, legend_labels, loc=1)
-        save_string = "stopping_efficiency_mix.pdf"
-        plt.savefig(pwd_fig+save_string, format='pdf', dpi=1000)
-        #plt.show()
-        plt.close()
-"""
-
+plt.figure(figsize=(12,10))
+for j in range(len(L)-1):
+    plt.plot(R, np.divide(1.0e3,solid_angle*FullE[j,:]/beam_total), color = colors[j], linestyle='-', marker='o', markersize=10, label = legend_labels[j])
+for j in range(len(L)-1):
+    plt.plot(R, np.divide(1.0e3,solid_angle*FFullE[j,:]/beam_total), color = colors[j], linestyle='--', marker='*', markersize=10, label = legend_labels[j])
+ax = plt.gca()
+handles, labels = ax.get_legend_handles_labels()
+ax.legend(handles, legend_labels[:-1], loc=1, title="Cone length (mm)")
+#plt.legend.set_title("Cone length (mm)")
+plt.title("Required source activity for 1 kHz", fontsize=20)
+plt.xlabel("Cone radius (mm)", fontsize=18)
+plt.ylabel("Activity (Bq)", fontsize=18)
+save_string = "NFlux.pdf"
+plt.savefig(save_string, format='pdf', dpi=1000)
+plt.show()
+plt.close()
